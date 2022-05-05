74adcef79353de1d344e15aae4e06bc8377958a7
Jason Felice <jasonfelice0@gmail.com>
2022-05-05 01:23:26 -0700

    Add test for deleting task

--- src/taskFunctionality.test.js
+++ src/taskFunctionality.test.js
@@ -48,5 +48,44 @@ describe('Adding and Removing one task and multiple tasks', () => {
     expect(deleteEditTask(myImage4, 1).length).toBe(taskArray.length = 3);
   });
 
-  
+  test('delete task being edited and check array length', () => {
+    const div = document.createElement('div');
+    div.classList.add('list-content-2');
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    const editText = document.createElement('input');
+    editText.setAttribute('value', 'Do Dishes');
+    const myImage4 = new Image();
+    myImage4.src = './delete.png';
+    myImage4.classList.add('delete');
+    div.appendChild(editText);
+    div.appendChild(checkBox);
+    div.appendChild(myImage4);
+    expect(deleteEditTask(myImage4, 1).length).toBe(taskArray.length = 2);
+  });
+  test('delete task that has already been deleted amd checking array length', () => {
+    const div = document.createElement('div');
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    const editText = document.createElement('input');
+    editText.setAttribute('value', 'Do Dishes');
+    const myImage4 = new Image();
+    myImage4.src = './delete.png';
+    div.appendChild(editText);
+    div.appendChild(checkBox);
+    div.appendChild(myImage4);
+    expect(deleteEditTask(myImage4, 1).length).toBe(taskArray.length = 2);
+  });
+  test('delete all remaining tasks', () => {
+    taskArray.forEach((task) => {
+      const div = document.createElement('div');
+      const checkBox = document.createElement('input');
+      checkBox.setAttribute('type', 'checkbox');
+      const editText = document.createElement('input');
+      editText.setAttribute('value', task.description);
+      div.appendChild(editText);
+      div.appendChild(checkBox);
+    });
+    expect(deleteTasks(1).length).toBe(taskArray.length = 0);
+  })
 });
